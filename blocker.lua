49d15d35bec933c53c51d40ca6a2ec5c062a0c16
Richard Garai <garric@okki.hu>
2024-01-14 11:08:05 +0100

    add all profession auras

--- blocker.lua
+++ blocker.lua
@@ -2,7 +2,19 @@ local _, hipe = ...
 
 local blocker = {
     blocked = {
-        [394011] = true,
+        [394003] = "Spark of Madness",
+        [388658] = "Suited For Smithing",
+        [394015] = "An Eye For Shine",
+        [394008] = "A Looker's Charm",
+        [394007] = "A Production-Grade Display",
+        [394016] = "Artist's Duds",
+        [394001] = "Sculpting Leather Finery",
+        [391312] = "Wrapped Up In Weaving",
+        [391775] = "What's Cookin', Good Lookin'?",
+        [394009] = "Fishing For Attention",
+        [394005] = "A Cultivator's Colors",
+        [394006] = "Rockin' Mining Gear",
+        [394011] = "Dressed To Kill",
     }
 }
 
