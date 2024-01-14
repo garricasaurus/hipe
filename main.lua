43f3e1236624fb31834759d0dca2b07eda526845
Richard Garai <garric@okki.hu>
2024-01-14 22:15:19 +0100

    fishing bugfix

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ local blocker = hipe.blocker
 ---@class Frame
 local frame = CreateFrame("Frame")
 frame:RegisterUnitEvent("UNIT_AURA", "player")
+frame:RegisterUnitEvent("UNIT_SPELLCAST_CHANNEL_STOP", "player")
 frame:RegisterEvent("PLAYER_REGEN_ENABLED")
 frame:SetScript("OnEvent", function(self, event, ...)
     self[event](self, ...)
@@ -16,6 +17,10 @@ function frame:UNIT_AURA(_, updateInfo)
             blocker:removeIfBlocked(aura.spellId)
         end
     end
+end
+
+function frame:UNIT_SPELLCAST_CHANNEL_STOP(_, _, _, _, spellId)
+    blocker:removeIfBlocked(spellId)    -- fishing
 end
 
 function frame:PLAYER_REGEN_ENABLED()
